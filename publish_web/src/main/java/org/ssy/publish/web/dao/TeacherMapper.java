c0febc7b0dc4b18e981b699438714ea5292baf1f
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-28 22:56:04 +0800

    提交

--- publish_web/src/main/java/org/ssy/publish/web/dao/TeacherMapper.java
+++
@@ -1,8 +0,0 @@
-package org.ssy.publish.web.dao;
-
-
-import org.ssy.publish.web.core.Mapper;
-import org.ssy.publish.web.model.Teacher;
-
-public interface TeacherMapper extends Mapper<Teacher> {
-}
--- publish_web/src/main/java/org/ssy/publish/web/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.ssy.publish.web.dao;
-
-
-import org.ssy.publish.web.core.Mapper;
-import org.ssy.publish.web.model.User;
-
-public interface UserMapper extends Mapper<User> {
-
-}
