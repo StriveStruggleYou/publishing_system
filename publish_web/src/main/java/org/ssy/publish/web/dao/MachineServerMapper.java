32d783ef4688a40fe51c1daf38ba0144ed37866c
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-28 22:55:38 +0800

    do

---
+++ publish_web/src/main/java/org/ssy/publish/web/dao/MachineServerMapper.java
@@ -0,0 +1,7 @@
+package org.ssy.publish.web.dao;
+
+import org.ssy.publish.web.core.Mapper;
+import org.ssy.publish.web.model.MachineServer;
+
+public interface MachineServerMapper extends Mapper<MachineServer> {
+}
--- publish_web/src/main/java/org/ssy/publish/web/dao/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package org.ssy.publish.web.dao;
-
-
-import org.ssy.publish.web.core.Mapper;
-import org.ssy.publish.web.model.Student;
-
-public interface StudentMapper extends Mapper<Student> {
-}
