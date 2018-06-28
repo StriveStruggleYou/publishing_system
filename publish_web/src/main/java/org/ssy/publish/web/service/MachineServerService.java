9131e9817cdac0e966a5f07ea847a9972b918a83
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-28 22:57:25 +0800

    提交基础数据信息

---
+++ publish_web/src/main/java/org/ssy/publish/web/service/MachineServerService.java
@@ -0,0 +1,11 @@
+package org.ssy.publish.web.service;
+import org.ssy.publish.web.model.MachineServer;
+import org.ssy.publish.web.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/06/28.
+ */
+public interface MachineServerService extends Service<MachineServer> {
+
+}
