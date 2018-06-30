f7e01bb399bdf4a596fc6b2ea717dc9965fc2c3c
StriveStruggleYou <StriveStruggleYou@163.com>
2018-06-30 23:33:36 +0800

    提交基础信息

---
+++ publish_web/src/main/java/org/ssy/publish/web/dao/ApplicationMapper.java
@@ -0,0 +1,7 @@
+package org.ssy.publish.web.dao;
+
+import org.ssy.publish.web.core.Mapper;
+import org.ssy.publish.web.model.Application;
+
+public interface ApplicationMapper extends Mapper<Application> {
+}
